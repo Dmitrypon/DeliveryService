97b0c56079d56649db9d7fd264c30cd634122edd
Dmitry Ponomar <mitrybox@gmail.com>
2025-02-19 21:41:21 +0300

    DeliveryService updated

---
+++ DeliveryService/Delivery.BusinessLogic/Services/Abstractions/IDeliveryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeliveryService.Delivery.BusinessLogic.Services.Delivery.Contracts.Dto;
+
+
+namespace DeliveryService.Delivery.BusinessLogic.Services.Delivery.Abstractions
+{
+    public interface IDeliveryService
+    {
+        /// <summary>
+        /// Создать доставку.
+        /// </summary>
+        /// <param name="createDeliveryDto"> Dto создаваемой доставки. </param>
+        public Task<Guid> CreateAsync(CreateDeliveryDto createDeliveryDto);
+
+        /// <summary>   
+        /// Получить доставку по id.
+        /// </summary>
+        /// <param name="id"> Идентификатор доставки. </param>
+        /// <returns> Dto доставки.</returns>
+        public Task<DeliveryDto> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Изменить доставку по id.
+        /// </summary>
+        /// <param name="id"> Иентификатор доставки. </param>
+        /// <param name="updateDeliveryDto"> Dto редактируемой доставки. </param>
+        public Task<bool> TryUpdateAsync(Guid id, updateDeliveryDto updateDeliveryDto);
+
+        /// <summary>
+        /// Удалить доставку.
+        /// </summary>
+        /// <param name="id"> Идентификатор доставки. </param>
+        public Task<bool> TryDeleteAsync(Guid id);
+    }
+}
--- DeliveryService/Program.cs
+++ DeliveryService/Program.cs
@@ -1,4 +1,14 @@
+using DeliveryService.Delivery.BusinessLogic.Services.Delivery.Abstractions;
+using DeliveryService.Delivery.BusinessLogic.Services.Delivery.Repositories;
+using DeliveryService.Delivery.BusinessLogic.Services.DeliveryService;
+using DeliveryService.Delivery.DataAccess.Data;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using DeliveryService;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.AddServiceDefaults();
 
 // Add services to the container.
 
@@ -6,10 +16,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
-var config = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplicationDataContext(builder.Configuration);
+
+builder.Services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+builder.Services.AddTransient<IDeliveryService, DeliveryServices>();
+
+
+//var apiDeliveryService = builder.AddProject<Projects.DeliveryService>("apiservice-delivery");
+
+//var apiOrderService = builder.AddProject<Projects.OrderService>("apiservice-order");
+
+//builder.AddProject<Projects.ApiGateway("webfrontend")
+//    .WithReference(apiDeliveryService)
+//    .WithReference(apiOrderService);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
+
+//app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,8 +55,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MigrateDatabase<DeliveryDBContext>();
+
 
 app.Run();
